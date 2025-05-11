eb22e261ef0d6dcf08e2d98f28c863f5ead3e571
KanakornMek <kanakorn.meka@gmail.com>
2025-05-11 22:02:10 +0700

    revert: remove unneccessary auth

--- frontend/native/app/(auth)/_layout.tsx
+++ frontend/native/app/(auth)/_layout.tsx
@@ -4,23 +4,7 @@ import { useAuth } from '@clerk/clerk-expo';
 import { View, ActivityIndicator } from 'react-native';
 
 export default function AuthLayout() {
-  const { isSignedIn, isLoaded } = useAuth();
-  const segments = useSegments(); // Ex. segments = ['(auth)', 'login']
-  const router = useRouter();
-
-  React.useEffect(() => {
-    if (!isLoaded) {
-      return;
-    }
-
-    const inAuthGroup = segments[0] === '(auth)';
-
-    if (isSignedIn && inAuthGroup) {
-      router.replace('/(tabs)'); // If user is signed in -> shouldn't be here -> redirect to the app
-    } else if (!isSignedIn && !inAuthGroup) {
-      router.replace('/(auth)/login'); // If user not signed in and trying to access main app (not in auth) -> redirect to login
-    }
-  }, [isSignedIn, isLoaded, segments, router]); // Re-run based on status conditions ()
+  const { isLoaded } = useAuth();
 
   if (!isLoaded) {
     return (
