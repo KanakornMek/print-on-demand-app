ad42aa9504e01699fa6f79d2b92aaeb50b43618d
Tatchai-Tee <themeturbo8@gmail.com>
2025-05-11 20:08:09 +0700

    Redesigned tabbar

--- frontend/native/app/(tabs)/_layout.tsx
+++ frontend/native/app/(tabs)/_layout.tsx
@@ -1,31 +1,53 @@
 import React from 'react';
 import { Tabs } from 'expo-router';
 import { Ionicons } from '@expo/vector-icons'; // Example icon library
+import Feather from '@expo/vector-icons/Feather';
 
 export default function TabLayout() {
   return (
     <Tabs
       screenOptions={{
-        // tabBarActiveTintColor: 'blue', 
-        // headerShown: false,
-      }}
-    >
+        tabBarActiveTintColor: '#f59e0b',
+        tabBarInactiveTintColor: "#b45309",
+        tabBarLabelStyle: {
+          fontSize: 12
+        },
+        tabBarStyle: {
+          borderTopColor: "#fde68a",
+          borderTopWidth: 1,
+          paddingTop: 4,
+          alignItems: 'center',
+          shadowColor: "#000",
+          shadowOpacity: 0.05,
+          shadowRadius: 10,
+          shadowOffset: { width: 0, height: -2 },
+        },
+        tabBarIconStyle: {
+          marginTop: 4
+        },
+        headerShown: false,
+      }}>
       <Tabs.Screen
         name="index"
         options={{
           title: 'Home',
-          tabBarIcon: ({ color, size }) => (
-            <Ionicons name="home-outline" color={color} size={size} />
-          ),
+          tabBarIcon: ({ color, size }) => <Feather name="home" color={color} size={size} />,
         }}
       />
+
+      <Tabs.Screen
+        name="cart"
+        options={{
+          title: 'Cart',
+          tabBarIcon: ({ color, size }) => <Feather name="shopping-cart" color={color} size={size} />,
+        }}
+      />
+
       <Tabs.Screen
         name="profile"
         options={{
           title: 'Profile',
-          tabBarIcon: ({ color, size }) => (
-            <Ionicons name="person-outline" color={color} size={size} />
-          ),
+          tabBarIcon: ({ color, size }) => <Feather name="user" color={color} size={size} />,
         }}
       />
     </Tabs>
---
+++ frontend/native/app/(tabs)/cart.tsx
@@ -0,0 +1,4 @@
+import { Text } from "react-native"
+export default function Cart() {
+  return <Text>Cart</Text>
+}
